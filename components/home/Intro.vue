ac028935b1c43de3a315902fca1f1826147282bc
themeprys <themeprys@gmail.com>
2025-01-28 18:45:52 +0800

    add font

--- components/home/Intro.vue
+++ components/home/Intro.vue
@@ -1,13 +1,21 @@
 <template>
   <section class="text-center bg-white" id="intro">
     <div class="container">
-      <div class="row justify-content-center py-5">
+      <div class="row justify-content-center pt-5">
         <div class="col-xl-9">
           <h2 class="h1">
             A celebration of culture and literacy
           </h2>
         </div>
       </div>
+      <div class="row justify-content-center pt-2 pb-5">
+        <div class="col-xl-9">
+          <p>
+            Singaraja Literary Festival (SLF) is a celebration of culture and literacy dedicated to reviving the rich literary heritage and intellectual traditions of Singaraja. Centered at Gedong Kirtya, Bali’s first lontar manuscript library, SLF blends art, research, and tradition through workshops, performances, exhibitions, and discussions.
+          </p>
+        </div>
+      </div>
+
       <div class="row py-5">
         <div class="col">
           <i class="fa-solid fa-building fa-4x"></i>
@@ -48,14 +56,6 @@
           <h3 class="pt-4 h3 fs-5">Art Installation</h3>
         </div>
       </div>      
-      <div class="row justify-content-center pt-2 pb-5">
-        <div class="col-xl-9">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde assumenda perferendis corrupti qui neque quibusdam provident eos odio voluptates. Minima nihil libero architecto culpa veritatis. Ex architecto nulla distinctio ratione!
-          </p>
-        </div>
-      </div>
-
     </div>
   </section>
 </template>
