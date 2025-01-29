d99f5aaec3999d0e36e25551deb9aa56d3b49165
themeprys <themeprys@gmail.com>
2025-01-29 14:34:59 +0800

    add content

---
+++ components/invitation-page.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+        <div class="row align-items-center g-lg-5 pt-5 pb-3">
+            <div class="col-lg-7 text-center text-lg-start">
+                <h1 class="display-4 fw-bold lh-1 mb-3">Get Your Invitation</h1>
+                <h3><strong>Attending Singaraja Literary Festival</strong></h3>
+                <p class="col-lg-10 fs-4">Step into the heart of creativity and culture at the Singaraja Literary Festival. Join us as we celebrate the power of words, connect with inspiring storytellers, and explore the beauty of Singaraja.</p>
+                <h3><strong>RSVP Now</strong></h3>
+                <p class="col-lg-10 fs-4">Whether you're a writer, reader, or art enthusiast, there's something for everyone. Save the date, and let’s create unforgettable memories together.
+                </p>                
+            </div>
+            <div class="col-md-10 mx-auto col-lg-5">
+                <img alt="Image" src="~/public/logo_SLF.svg" width="400px">
+            </div>
+        </div>          
+        <div class="row">
+            <div class="col fs-5">
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
