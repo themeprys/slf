50e216799258e6a5f64580b8bbc6b24785fd4917
themeprys <themeprys@gmail.com>
2025-04-01 10:34:03 +0800

    fix 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,20 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ['~/public/main.css'],
-    devtools: { enabled: true },
-    ssr: true,
-    runtimeConfig: {
-        public: {
-            cosmicBucketSlug: process.env.COSMIC_BUCKET_SLUG,
-            cosmicReadKey: process.env.COSMIC_READ_KEY,
-            cosmicWriteKey: process.env.COSMIC_WRITE_KEY,
-        }
-    },
-    nitro: {
-        preset: 'vercel'
-    }
+  css: ['~/public/main.css'],
+  devtools: { enabled: true },
+  ssr: true,
+
+  runtimeConfig: {
+      public: {
+          cosmicBucketSlug: process.env.COSMIC_BUCKET_SLUG,
+          cosmicReadKey: process.env.COSMIC_READ_KEY,
+          cosmicWriteKey: process.env.COSMIC_WRITE_KEY,
+      }
+  },
+
+  nitro: {
+      preset: 'vercel'
+  },
+
+  compatibilityDate: '2025-04-01'
 })
